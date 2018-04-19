72645ec4fab96bc67c230d494acc050e9f2939d3
Scott <scott@tams.tec>
2018-04-19 19:56:21 -0400

    init

---
+++ play_audio_file.py
@@ -0,0 +1,23 @@
+# SimpleAudio and PyAudio only accept .wav files, use PyGame
+from pygame import mixer
+from magic import detect_from_filename as get_filetype
+
+mixer.init(frequency=44100)
+valid_filetypes = (
+    "audio/x-wav",
+    "audio/x-flac",
+    "audio/ogg"
+)
+
+def play_audio_file(filename: str):
+    """Plays an audio file."""
+    filetype = get_filetype(filename).mime_type
+    if filetype in valid_filetypes:
+        mixer.music.load(filename)
+        mixer.music.play()
+    else:
+        raise ValueError(
+            f"Filetype {filetype} of audio file {filename} isn't playable."
+            + " This script can only handle wav, flac, and ogg filetypes, "
+            + "due to the underlying library capabilities."
+        )
