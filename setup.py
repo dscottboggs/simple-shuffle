d6a1d861be3ea744cdda8727062b2be8968d3557
D. Scott Boggs <scott@tams.tech>
2018-04-20 17:41:48 -0400

    That should finish setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,20 @@ from git.repo.base import Repo
 from os.path import dirname, realpath
 
 vcs = Repo(dirname(realpath(__file__)))
-
-help(vcs)
-exit()
+urls = [u for u in vcs.remote().urls]
+if len(urls) < 1:
+    raise NotImplementedError()
 
 setup(
     name="Simple Shuffle",
-    version="0.0.1",
+    version="0.0.%d" % len(vcs.iter_commands()),
     author="D. Scott Boggs",
     author_email="scott@tams.tech",
     description="Shuffles a folder of music. That is all.",
     license="GPLv3",
-    keywords="music player shuffle curses ncurses minimal simple"
+    keywords="music player shuffle curses ncurses minimal simple",
+    url=urls[0],
+    packages=["simple_shuffle"],
+    install_requires=["pygame", "mutagen", "file-magic"],
+    setup_requires=["gitpython"]
 )
