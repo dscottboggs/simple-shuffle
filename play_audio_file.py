b3cb89d90be85a7b97fcdc3b5870323a60f9c3f2
D. Scott Boggs <scott@tams.tech>
2018-04-20 13:34:47 -0400

    trying to figure out how to get the window dimensions

--- play_audio_file.py
+++ play_audio_file.py
@@ -208,6 +208,7 @@ class Player():
                 y: y coord
             }
         """
+        print(screen.getyx())
         screen.clear()
         for txt, coords in text(screen).items():
             screen.addstr(
