2dc90a9429b3d42bfc25d9ddb386f02006e193b8
scott <scott@tams.tech>
2018-04-20 12:35:24 -0400

    Added logging

--- play_audio_file.py
+++ play_audio_file.py
@@ -3,15 +3,18 @@
 # SimpleAudio and PyAudio only accept .wav files, use PyGame
 from pygame import mixer
 from magic import detect_from_filename as get_filetype
-from os import environ, access
+from os import access
 from os.path import isdir, basename
-from os import X_OK as FILE_IS_EXECUTABLE
+from os import R_OK as FILE_IS_READABLE
 from mutagen.easyid3 import EasyID3
 from mutagen.id3._util import ID3NoHeaderError
 import curses
 from binascii import hexlify
 from strict_hint import strict
+from config import Config
 
+log = Config.logger
+help(log)
 mixer.init(frequency=44100)
 valid_filetypes = (
     "audio/x-wav",
@@ -53,47 +56,70 @@ class Player():
         Makes several attempts at picking fewer tags before finally displaying
         the filename as a fallback.
         """
+        outtxt: str = ''
         try:
             tags = EasyID3(self.current_file)
         except ID3NoHeaderError:
             # This is raised if there is absolutely no ID3 information in
             # the file
-            return self.current_file.split('/')[-1:]
+            try:
+                outtxt = basename(
+                    self.current_file
+                ).rsplit('.', maxsplit=1)[0]
+                log.debug("Song info: %s", outtxt)
+                return outtxt
+            except IndexError:
+                # the filename has no extension to trim
+                outtxt = basename(self.current_file)
+                log.debug("Song info: %s", outtxt)
+                return outtxt
         try:
             # the default output
-            return "%s by %s,\nTrack %d out of %d from their album, %s." % (
+            outtxt = "%s by %s,\nTrack %d out of %d from their album, %s." % (
                 tags['title'][0],
                 tags['artist'][0],
                 int(tags['tracknumber'][0].split('/', maxsplit=1)[0]),
                 int(tags['tracknumber'][0].split('/', maxsplit=1)[1]),
                 tags['album'][0]
             )
+            log.debug("Song info: %s", outtxt)
+            return outtxt
         except (KeyError, ValueError):
             try:
                 # Perhaps the tracknumber or album tags are missing, try
                 # displaying just the song title and the artist
-                return "%s by %s" % (tags['title'][0], tags['artist'][0])
+                outtxt = "%s by %s" % (tags['title'][0], tags['artist'][0])
+                log.debug("Song info: %s", outtxt)
+                return outtxt
             except (KeyError, ValueError):
                 try:
                     # perhaps there's still a title tag, try displaying that.
-                    return tags['title'][0]
+                    outtxt = tags['title'][0]
+                    log.debug("Song info: %s", outtxt)
+                    return outtxt
                 except (KeyError, ValueError):
                     # Just return the filename...
                     try:
-                        return basename(
+                        outtxt = basename(
                             self.current_file
                         ).rsplit('.', maxsplit=1)[1]
+                        log.debug("Song info: %s", outtxt)
+                        return outtxt
                     except IndexError:
                         # the filename has no extension to trim
-                        return basename(self.current_file)
+                        outtxt = basename(self.current_file)
+                        log.debug("Song info: %s", outtxt)
+                        return outtxt
 
     @strict
     def begin_playback(self):
         """Play an audio file."""
         filetype = get_filetype(self.current_file).mime_type
         if filetype in valid_filetypes:
+            log.debug("Beginning playback of %s", self.current_file)
             mixer.music.load(self.current_file)
             mixer.music.play()
+            self.paused = False
         else:
             raise ValueError(
                 "Filetype %s of audio file %s isn't playable." % (
