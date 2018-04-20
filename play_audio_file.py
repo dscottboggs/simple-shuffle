e1e1301f249dfd1a6cc56ce757bc4af98c4a002e
D. Scott Boggs <scott@tams.tech>
2018-04-20 16:44:08 -0400

    This looks better. I'll have to figure out how to update that.

--- play_audio_file.py
+++ play_audio_file.py
@@ -242,7 +242,7 @@ class Player():
             })
         text.update({     # I'll come back to this
             str(float(mixer.music.get_pos()/1000)) + " seconds": {
-                'x': int(maxcolumns/3),
+                'x': 2,
                 'y': int(maxlines) - 1
             }
         })
