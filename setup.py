41be5b2b381c5bd0ab9529ee7c7f839bf678ac16
D. Scott Boggs <scott@tams.tech>
2018-04-21 03:35:36 -0400

    added printout of current version cuz I wanna see

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ vcs = Repo(dirname(realpath(__file__)))
 urls = [u for u in vcs.remote().urls]
 if len(urls) < 1:
     raise NotImplementedError()
+versionnum = len([c for c in vcs.iter_commits()])
+print("Current version 0.0.%d" % versionnum)
 
 setup(
     name="Simple Shuffle",
-    version="0.0.%d" % len([c for c in vcs.iter_commits()]),
+    version="0.0.%d" % versionnum,
     author="D. Scott Boggs",
     author_email="scott@tams.tech",
     description="Shuffles a folder of music. That is all.",
