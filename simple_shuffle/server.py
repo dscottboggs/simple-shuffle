b00d3c8cb5e94278679e06c078362b3ed47a074d
D. Scott Boggs <scott@tams.tech>
2018-04-22 21:16:37 -0400

    Added launcher script

--- simple_shuffle/server.py
+++ simple_shuffle/server.py
@@ -2,39 +2,17 @@
 """Begin the simple_shuffle and watch for commands on a port."""
 from flask import Flask, request
 from simple_shuffle.player import Player
-import click as cli
 from os.path import join as getpath
 from os import environ
-from os import sep as root
 from json import dumps
 # from multiprocessing import Pool
 app = Flask(__name__)
 # pool = Pool(1)
 player = Player(
-    getpath(root, 'home', environ['USER'], 'Music'),
+    environ['SIMPLE_SHUFFLE_FOLDER']
+    or getpath(environ['HOME'], 'Music'),
     autoplay=True
 )
-
-
-@cli.command("shuffle-server")
-@cli.argument("shuffle_folder", required=False)
-def main(*args, **kwargs):
-    """The shuffle-server script shuffles a folder of flac and ogg files.
-
-    It does a true shuffle of all of the songs in the folder, without repeats,
-    unless you've got duplicates. Specify the folder to be shuffled on the
-    command line just after the name. By default it shuffles /home/$USER/Music.
-
-    The server version sets up a Flask server to respond to actions on a port
-    through standard HTTP requests.
-    """
-    if kwargs['shuffle_folder'] is None:
-        return Player(getpath(root, 'home', environ['USER'], 'Music'))
-    else:
-        return Player(kwargs['shuffle_folder'])
-
-
-# player = main()
 
 
 @app.route("/pause_unpause")
